cee5426b6d66db1c24c0587c776c5ac9315e79f1
BC-macair020 <bc-macair020@BC-macair020s-MacBook-Air.local>
2021-11-22 21:52:13 +0200

    fetching data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
+import React, { Component } from "react";
+import axios from "axios";
 import "./App.css";
 
-function App() {
-  return <div className="App">App will be here</div>;
+class App extends Component {
+  state = {
+    data: [],
+  };
+
+  componentDidMount() {
+    axios.get("https://restcountries.com/v3.1/all").then((res) => {
+      this.setState({ data: res.data });
+      // console.log(this.state.data);
+    });
+  }
+  render() {
+    return (
+      <div className="countries">
+        {this.state.data.map((item) => (
+          <div key={item.name.common} className="country">
+            <h2>{item.name.common}</h2>
+            <p>{item.capital}</p>
+            <img src={item.flags.png} />
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
