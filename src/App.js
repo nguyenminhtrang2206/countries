b959e4c62337ca5c765646d015ec76c6be9012aa
BC-macair020 <bc-macair020@BC-macair020s-MacBook-Air.local>
2021-11-23 10:29:27 +0200

    adding languages, changing the API

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import axios from "axios";
 import "./App.css";
+import number from "easy-number-formatter";
 
 class App extends Component {
   state = {
@@ -8,19 +9,30 @@ class App extends Component {
   };
 
   componentDidMount() {
-    axios.get("https://restcountries.com/v3.1/all").then((res) => {
-      this.setState({ data: res.data });
-      // console.log(this.state.data);
-    });
+    axios
+      .get(
+        "https://restcountries.com/v2/all?fields=name,flags,capital,languages,population"
+      )
+      .then((res) => {
+        this.setState({ data: res.data });
+        console.log(this.state.data);
+      });
   }
   render() {
     return (
       <div className="countries">
         {this.state.data.map((item) => (
-          <div key={item.name.common} className="country">
-            <h2>{item.name.common}</h2>
+          <div key={item.name} className="country">
+            <h2>{item.name}</h2>
             <p>{item.capital}</p>
-            <img src={item.flags.png} />
+            <img src={item.flags.png} alt="flag" />
+            <p>Population: {number.formatNumber(item.population)}</p>
+            <p>
+              Language(s):{" "}
+              {item.languages.map((lang) => (
+                <span>{lang.name} </span>
+              ))}
+            </p>
           </div>
         ))}
       </div>
