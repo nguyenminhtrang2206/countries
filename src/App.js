4562bc7c93d3aa9b89595c70f3aa3b285b1cc39e
BC-macair020 <bc-macair020@BC-macair020s-MacBook-Air.local>
2021-11-23 14:15:36 +0200

    adding search-bar

--- src/App.js
+++ src/App.js
@@ -6,37 +6,83 @@ import number from "easy-number-formatter";
 class App extends Component {
   state = {
     data: [],
+    searchInput: "",
+    isLoading: true,
+  };
+
+  searchHandler = (event) => {
+    this.setState({
+      searchInput: event.target.value,
+    });
+    console.log(this.state.searchInput);
   };
 
   componentDidMount() {
     axios
       .get(
-        "https://restcountries.com/v2/all?fields=name,flags,capital,languages,population"
+        "https://restcountries.com/v2/all?fields=name,flags,capital,languages,population,currencies"
       )
       .then((res) => {
-        this.setState({ data: res.data });
-        console.log(this.state.data);
+        this.setState({ isLoading: false, data: res.data });
       });
   }
+
   render() {
-    return (
-      <div className="countries">
-        {this.state.data.map((item) => (
-          <div key={item.name} className="country">
-            <h2>{item.name}</h2>
-            <p>{item.capital}</p>
-            <img src={item.flags.png} alt="flag" />
-            <p>Population: {number.formatNumber(item.population)}</p>
-            <p>
-              Language(s):{" "}
-              {item.languages.map((lang) => (
-                <span>{lang.name} </span>
+    if (this.state.isLoading) {
+      return (
+        <div className="loading">
+          <p>Wait, I am loading...</p>
+          <div className="loader"></div>
+        </div>
+      );
+    }
+
+    if (!this.state.isLoading) {
+      return (
+        <div className="max-width">
+          <input
+            className="search-bar"
+            type="text"
+            name="search"
+            placeholder="Search"
+            onChange={this.searchHandler.bind(this)}
+          />
+
+          <div className="countries">
+            {this.state.data
+              .filter((item) => {
+                return item.name
+                  .toLowerCase()
+                  .includes(this.state.searchInput.toLowerCase());
+              })
+              .map((item) => (
+                <div key={item.name} className="country">
+                  <h2>{item.name}</h2>
+                  <p>{item.capital}</p>
+                  <img src={item.flags.png} alt="flag" />
+                  <p className="population">
+                    Population: {number.formatNumber(item.population)}
+                  </p>
+                  <p>
+                    Language(s):
+                    {item.languages.map((lang, i) => (
+                      <span key={i}>{lang.name} </span>
+                    ))}
+                  </p>
+                  <p>
+                    Currencies:{" "}
+                    {item.currencies.map((curr, i) => (
+                      <span key={i}>
+                        {curr.name} - {curr.symbol}
+                      </span>
+                    ))}
+                  </p>
+                </div>
               ))}
-            </p>
           </div>
-        ))}
-      </div>
-    );
+        </div>
+      );
+    }
   }
 }
 
